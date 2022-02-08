286030f98b00818e4a931c10c49215f73e869d34
ankitrkumar <kumar.877@buckeyemail.osu.edu>
2022-02-08 15:59:12 -0500

    Added counter

--- Zelda Game/KeyBoardController.cs
+++ Zelda Game/KeyBoardController.cs
@@ -8,6 +8,7 @@ namespace Zelda_Game
         KeyboardState userInput;
         private Game1 game;
         private List<IEnviornment> List;
+        private int i = 0;
         public KeyBoardController(Game1 _game, List<IEnviornment> blockList)
         {
             game = _game;
