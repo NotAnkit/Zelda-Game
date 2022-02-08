b3ab27a150f45dd5c7818e04ba55735ccb62e74a
rama-naboulsi <naboulsi.3@osu.edu>
2022-02-08 09:42:40 -0500

    updated jelly class

--- Zelda Game/Jelly.cs
+++ Zelda Game/Jelly.cs
@@ -3,27 +3,48 @@ using Microsoft.Xna.Framework;
 
 namespace Zelda_Game
 {
+    /*animated and moves left/right*/
     public class Jelly : IEnemy
     {
         public Texture2D Texture;
+        private int currentFrame;
+        private int totalFrames;
+        private float spriteSpeed;
         private int windowHeight;
         private int windowWidth;
+        private Vector2 position;
 
         public Jelly(Game1 game)
         {
             Texture = game.Content.Load<Texture2D>("ItemSheet");
+            currentFrame = 0;
+            totalFrames = 2;
+            spriteSpeed = 3f;
             windowHeight = game._graphics.PreferredBackBufferHeight;
             windowWidth = game._graphics.PreferredBackBufferWidth;
+            position = new Vector2(windowWidth / 2, windowHeight / 2);
         }
 
         public void Update()
         {
+            currentFrame++;
+            if (currentFrame == totalFrames)
+                currentFrame = 0;
+
+            position.X += spriteSpeed;
+            if (position.X > windowWidth)
+                position.X = 0;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            Rectangle sourceRectangle = new Rectangle(1, 10, 8, 16);
-            Rectangle destinationRectangle = new Rectangle(windowWidth / 2, windowHeight / 2, 8, 16);
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 8, 16);
+
+            if (currentFrame == 0)
+                sourceRectangle = new Rectangle(1, 11, 8, 16);
+            else
+                sourceRectangle = new Rectangle(11, 11, 8, 16);
 
             spriteBatch.Begin();
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
