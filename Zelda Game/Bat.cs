78f78cba1840892cb1ee5339fdcea47268abc94d
rama-naboulsi <naboulsi.3@osu.edu>
2022-02-09 09:59:47 -0500

    animate and move bat

--- Zelda Game/Bat.cs
+++ Zelda Game/Bat.cs
@@ -1,29 +1,74 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace Zelda_Game
 {
+    /*animate and move arbitrarily across screen*/
     public class Bat : IEnemy
     {
         public Texture2D Texture;
+        private int currentFrame;
+        private int totalFrames;
+        private float spriteSpeed;
         private int windowHeight;
         private int windowWidth;
+        private Vector2 position;
 
         public Bat(Game1 game)
         {
             Texture = game.Content.Load<Texture2D>("ItemSheet");
+            currentFrame = 0;
+            totalFrames = 3;
+            spriteSpeed = 3f;
             windowHeight = game._graphics.PreferredBackBufferHeight;
             windowWidth = game._graphics.PreferredBackBufferWidth;
+            position = new Vector2(windowWidth / 2, windowHeight / 2);
         }
 
         public void Update()
         {
+            currentFrame++;
+            if (currentFrame == totalFrames)
+                currentFrame = 0;
+
+            Random Rnd = new Random();
+            int num = Rnd.Next();
+            if (num % 4 == 0)
+            {
+                position.X += spriteSpeed;
+                if (position.X > windowWidth)
+                    position.X = 0;
+            }
+            else if (num % 4 == 1)
+            {
+                position.Y += spriteSpeed;
+                if (position.Y > windowHeight)
+                    position.Y = 0;
+            }
+            else if (num % 4 == 2)
+            {
+                position.X -= spriteSpeed;
+                if (position.X < 0)
+                    position.X = windowWidth;
+            }
+            else
+            {
+                position.Y -= spriteSpeed;
+                if (position.Y < 0)
+                    position.Y = windowHeight;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            Rectangle sourceRectangle = new Rectangle(180, 10, 16, 16);
-            Rectangle destinationRectangle = new Rectangle(windowWidth / 2, windowHeight / 2, 16, 16);
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 16, 16);
+
+            if (currentFrame == 0)
+                sourceRectangle = new Rectangle(180, 11, 16, 16);
+            else
+                sourceRectangle = new Rectangle(200, 11, 16, 16);
 
             spriteBatch.Begin();
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
