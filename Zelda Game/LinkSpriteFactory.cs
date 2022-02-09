2fd7c5426205c1937034e4b163cc87fffb47e5b9
kirenmanoharan <manoharan.25@buckeyemail.osu.edu>
2022-02-09 16:56:49 -0500

    warning error

--- Zelda Game/LinkSpriteFactory.cs
+++ Zelda Game/LinkSpriteFactory.cs
@@ -3,79 +3,79 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Zelda_Game
 {
-	public class LinkSpriteFactory
-	{
-		private Texture2D LinkSpritesheet;
-		// More private Texture2Ds follow
-		// ...
+    public class LinkSpriteFactory
+    {
+        private Texture2D LinkSpritesheet;
+        // More private Texture2Ds follow
+        // ...
 
-		private static LinkSpriteFactory instance = new LinkSpriteFactory();
+        private static LinkSpriteFactory instance = new LinkSpriteFactory();
 
-		public static LinkSpriteFactory Instance
-		{
-			get
-			{
-				return instance;
-			}
-		}
+        public static LinkSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
 
-		private LinkSpriteFactory()
-		{
-		}
+        private LinkSpriteFactory()
+        {
+        }
 
-		public void LoadAllTextures(ContentManager content)
-		{
-			LinkSpritesheet = content.Load<Texture2D>("LinkSheet");
-			// More Content.Load calls follow
-			//...
-		}
+        public void LoadAllTextures(ContentManager content)
+        {
+            LinkSpritesheet = content.Load<Texture2D>("LinkSheet");
+            // More Content.Load calls follow
+            //...
+        }
 
-		public ISprite LinkDownAnimationSprite()
-		{
-			return new LinkDownAnimation(LinkSpritesheet);
-		}
+        public ISprite LinkDownAnimationSprite()
+        {
+            return new LinkDownAnimation(LinkSpritesheet);
+        }
 
-		public ISprite LinkDownIdleSprite()
-		{
-			return new LinkDownIdle(LinkSpritesheet);
-		}
+        public ISprite LinkDownIdleSprite()
+        {
+            return new LinkDownIdle(LinkSpritesheet);
+        }
 
-		public ISprite LinkLeftAnimationSprite()
-		{
-			return new LinkLeftAnimation(LinkSpritesheet);
-		}
+        public ISprite LinkLeftAnimationSprite()
+        {
+            return new LinkLeftAnimation(LinkSpritesheet);
+        }
 
-		public ISprite LinkLeftIdleSprite()
-		{
-			return new LinkLeftIdle(LinkSpritesheet);
-		}
+        public ISprite LinkLeftIdleSprite()
+        {
+            return new LinkLeftIdle(LinkSpritesheet);
+        }
 
-		public ISprite LinkRightAnimationSprite()
-		{
-			return new LinkRightAnimation(LinkSpritesheet);
-		}
+        public ISprite LinkRightAnimationSprite()
+        {
+            return new LinkRightAnimation(LinkSpritesheet);
+        }
 
-		public ISprite LinkRightIdleSprite()
-		{
-			return new LinkRightIdle(LinkSpritesheet);
-		}
+        public ISprite LinkRightIdleSprite()
+        {
+            return new LinkRightIdle(LinkSpritesheet);
+        }
 
-		public ISprite LinkUpAnimationSprite()
-		{
-			return new LinkUpAnimation(LinkSpritesheet);
-		}
+        public ISprite LinkUpAnimationSprite()
+        {
+            return new LinkUpAnimation(LinkSpritesheet);
+        }
 
-		public ISprite LinkUpIdleSprite()
-		{
-			return new LinkUpIdle(LinkSpritesheet);
-		}
+        public ISprite LinkUpIdleSprite()
+        {
+            return new LinkUpIdle(LinkSpritesheet);
+        }
 
-		public ISprite LinkWoodSwordDownAnimationSprite()
-		{
-			return new LinkWoodSwordDownAnimation(LinkSpritesheet);
-		}
+        public ISprite LinkWoodSwordDownAnimationSprite()
+        {
+            return new LinkWoodSwordDownAnimation(LinkSpritesheet);
+        }
 
-		// More public ISprite returning methods follow
-		// ...
-	}
+        // More public ISprite returning methods follow
+        // ...
+    }
 }
