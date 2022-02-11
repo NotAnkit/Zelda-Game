12630a703f27f32443585c8e31081068fc1f8391
mohamedelzubeir <elzubeir.2@buckeyemail.osu.edu>
2022-02-11 16:15:57 -0500

    added

---
+++ Zelda Game/ClockItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Zelda_Game
+{
+    public class ClockItem : IItem
+    {
+        public Texture2D Texture;
+        private int windowHeight;
+        private int windowWidth;
+
+        public CLockItem(Game1 game)
+        {
+            Texture = game.Content.Load<Texture2D>("WeaponSheet");
+            windowHeight = game._graphics.PreferredBackBufferHeight;
+            windowWidth = game._graphics.PreferredBackBufferWidth;
+        }
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(58, 0, 11, 16);
+            Rectangle destinationRectangle = new Rectangle(0, 0, 32, 32);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+    }
+}
---
+++ Zelda Game/FairyItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Zelda_Game
+{
+    public class FairyItem : IItem
+    {
+        public Texture2D Texture;
+        private int windowHeight;
+        private int windowWidth;
+
+        public FairyItem(Game1 game)
+        {
+            Texture = game.Content.Load<Texture2D>("WeaponSheet");
+            windowHeight = game._graphics.PreferredBackBufferHeight;
+            windowWidth = game._graphics.PreferredBackBufferWidth;
+        }
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(40, 0, 8, 16);
+            Rectangle destinationRectangle = new Rectangle(0, 0, 32, 32);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+    }
+}
