a36a4541a3727d011e6e2e6bfcd1a9c921ca03ac
rama-naboulsi <naboulsi.3@osu.edu>
2022-02-11 16:58:37 -0500

    fixed jelly movement

--- Zelda Game/Jelly.cs
+++ Zelda Game/Jelly.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace Zelda_Game
 {
@@ -18,22 +19,53 @@ namespace Zelda_Game
         {
             Texture = game.Content.Load<Texture2D>("ItemSheet");
             currentFrame = 0;
-            totalFrames = 2;
-            spriteSpeed = 3f;
+            totalFrames = 30;
+            spriteSpeed = 1f;
             windowHeight = game._graphics.PreferredBackBufferHeight;
             windowWidth = game._graphics.PreferredBackBufferWidth;
             position = new Vector2(windowWidth / 2, windowHeight / 2);
         }
 
+        int movementCounter = 0;
+        int num = 0;
         public void Update()
         {
             currentFrame++;
             if (currentFrame == totalFrames)
                 currentFrame = 0;
 
-            position.X += spriteSpeed;
-            if (position.X > windowWidth)
-                position.X = 0;
+            Random rnd = new Random();
+            movementCounter++;
+            if (movementCounter == 15)
+            {
+                num = rnd.Next();
+                movementCounter = 0;
+            }
+
+            if (num % 4 == 0)
+            {
+                position.X += spriteSpeed;
+                if (position.X > windowWidth)
+                    position.X = 0;
+            }
+            else if (num % 4 == 1)
+            {
+                position.Y += spriteSpeed;
+                if (position.Y > windowHeight)
+                    position.Y = 0;
+            }
+            else if (num % 4 == 2)
+            {
+                position.X -= spriteSpeed;
+                if (position.X < 0)
+                    position.X = windowWidth;
+            }
+            else
+            {
+                position.Y -= spriteSpeed;
+                if (position.Y < 0)
+                    position.Y = windowHeight;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -41,10 +73,10 @@ namespace Zelda_Game
             Rectangle sourceRectangle;
             Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 16, 32);
 
-            if (currentFrame == 0)
-                sourceRectangle = new Rectangle(1, 11, 8, 16);
+            if (currentFrame <= totalFrames/2)
+                sourceRectangle = new Rectangle(1,11, 8, 16);
             else
-                sourceRectangle = new Rectangle(11, 11, 8, 16);
+                sourceRectangle = new Rectangle(10, 11, 8, 16);
 
             
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
