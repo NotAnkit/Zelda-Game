6051adb966119b602b1a9dfee9095bba128a1bde
mohamedelzubeir <elzubeir.2@buckeyemail.osu.edu>
2022-02-11 10:19:32 -0500

    Added

---
+++ Zelda Game/CompassItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Zelda_Game
+{
+    public class Compass : IItem
+    {
+        public Texture2D Texture;
+        private int windowHeight;
+        private int windowWidth;
+
+        public Compass(Game1 game)
+        {
+            Texture = game.Content.Load<Texture2D>("ItemSheet");
+            windowHeight = game._graphics.PreferredBackBufferHeight;
+            windowWidth = game._graphics.PreferredBackBufferWidth;
+        }
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(1001, 28, 16, 16);
+            Rectangle destinationRectangle = new Rectangle(100, 100, 32, 32);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+    }
+}
---
+++ Zelda Game/IItem.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Zelda_Game
+{
+    public interface IItem
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
