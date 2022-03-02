1a65ec2a3c7f6fb0dc4cd51faaa42b0836a1d531
ankitrkumar <kumar.877@buckeyemail.osu.edu>
2022-03-02 13:07:35 -0500

    Fixed bottom doors

--- Zelda Game/BottomSealed.cs
+++ Zelda Game/BottomSealed.cs
@@ -16,7 +16,7 @@ namespace Zelda_Game
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
             Rectangle sourceRectangle = new Rectangle(915, 110, 32, 32);
-            Rectangle destinationRectangle = new Rectangle(215, 282, 65, 63);
+            Rectangle destinationRectangle = new Rectangle(217, 282, 65, 63);
 
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
         }
