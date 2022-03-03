afba1aa96094d78c725d1db8e73188d3b87ab8b4
kirenmanoharan <manoharan.25@buckeyemail.osu.edu>
2022-03-03 16:39:50 -0500

    Trying to implement collision detection

--- Zelda Game/Blocks/PushableBlock.cs
+++ Zelda Game/Blocks/PushableBlock.cs
@@ -6,14 +6,20 @@ namespace Zelda_Game
     public class PushableBlock : IEnvironment
     {
         public Texture2D Texture;
+        public Vector2 location;
 
         public PushableBlock(Game1 game)
         {
             Texture = game.Content.Load<Texture2D>("RoomSheet");
         }
 
+        //public Rectangle PushableRectangle
+        //{
+        //    get { return new Rectangle((int)location.X, (int)location.Y, 32, 32); }
+        //}
+
         public void Update()
-        {
+        {      
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
@@ -25,5 +31,6 @@ namespace Zelda_Game
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
 
         }
+
     }
 }
--- Zelda Game/Link.cs
+++ Zelda Game/Link.cs
@@ -8,12 +8,20 @@ namespace Zelda_Game
     public class Link
     {
         public string direction = "right";
+        //public Rectangle PushableRectangle;
         private Vector2 position;
         private Vector2 itemPosition;
         public ILinkState currentState;     
         private bool useItem;
         private int animationCount;
         private ISprite item;
+
+        public object Rectangle { get; internal set; }
+
+        public Rectangle LinkRectangle
+        {
+            get { return new Rectangle((int)position.X, (int)position.Y, 32, 32); }
+        }
 
         public Link(Vector2 location)
         {
@@ -40,6 +48,12 @@ namespace Zelda_Game
                 }
 
             }
+
+            //if (LinkRectangle.Intersects(PushableRectangle)) {
+            //    position.X = 0;
+            //    position.Y = 0;
+            
+            //}
         }
 
         public void UseItem(string itemName)
