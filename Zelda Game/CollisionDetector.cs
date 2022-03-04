cea0c52827b9763c438b0d625570e1aad35ad8bd
rama-naboulsi <naboulsi.3@osu.edu>
2022-03-04 09:10:34 -0500

    collision

---
+++ Zelda Game/CollisionDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Zelda_Game
+{
+    public class CollisionDetector
+    {
+        public CollisionDetector()
+        {
+            //player loop through enemies
+
+            //player loop through blocks
+
+            //player loop through items
+            //can just use Rectangle.Intersects because side doesn't matter
+        }
+    }
+}
---
+++ Zelda Game/PlayerBlockCollision.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zelda_Game
+{
+    public class PlayerBlockCollision
+    {
+        public PlayerBlockCollision()
+        {
+        }
+
+        public String Collision(Rectangle linkRectangle, Rectangle blockRectangle)
+        {
+            String collisionType = "none";
+            Rectangle overlap = Rectangle.Intersect(linkRectangle, blockRectangle);
+
+            return collisionType;
+        }
+    }
+}
