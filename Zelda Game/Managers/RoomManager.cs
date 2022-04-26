fa65f8a6270769b96ffd47039a16afb4f7681de6
ankitrkumar <kumar.877@buckeyemail.osu.edu>
2022-04-25 23:23:59 -0400

    Added docs

--- Zelda Game/Managers/RoomManager.cs
+++ Zelda Game/Managers/RoomManager.cs
@@ -65,7 +65,7 @@ namespace Zelda_Game
             roomList.Add(new KeyValuePair<int, int>(1, 2), new Room(Game.Content.Load<Level>("Room1"), Game, random));
             roomList.Add(new KeyValuePair<int, int>(1, 1), new Room(Game.Content.Load<Level>("basement"), Game, false));
 
-            room = game1.Content.Load<Level>("Room10");
+            room = game1.Content.Load<Level>("Room4");
             roomData = new Room(room, game1, random);
         }
 
